7c3e4a09a04572318f82ae15d36ae535540273d7
kostas vlachas <kostas.vl@hotmail.gr>
2023-11-07 11:35:08 +0200

    up

--- website/__init__.py
+++ website/__init__.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 
 def create_app():
     app = Flask(__name__)
--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,35 @@
-from flask import Blueprint
+from xmlrpc.client import boolean
+from flask import Blueprint, render_template, request, flash
+from networkx import cartesian_product
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return "<p>Login</p>"
+    return render_template("login.html", boolean=True)
 
 @auth.route('/logout')
 def logout():
     return "<p>Logout</p>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
-    return "<p>Sign Up</p>"
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4: 
+            flash('Email must be greater than 3 characters.', category='error')
+        elif len(firstName) < 2:
+            flash('First name must be greater than 1 character.', category='error')
+        elif password1 != password2:
+            flash('Passwords don\'t match.', category='error')
+        elif len(password1) < 7:
+            flash('Password must have at least 7 characters.', category='error')
+        else:
+            flash('Account created!', category='success')
+            
+
+    return render_template("sign_up.html")
